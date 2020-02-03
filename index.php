f6bc479dbf1548182e149019a2565aeb09298e4d
hasina <hasinarandriamora97@gmail.com>
2020-02-03 15:08:41 +0100

    html

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-hello
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Pratum</title>
+    <link rel="stylesheet" href="app.scss">
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>Pratum Application</h1>
+</body>
+</html>
