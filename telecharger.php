4b065e0878d6d4cd4b8ea1562585390d0e098a78
hasina <hasinarandriamora97@gmail.com>
2020-02-04 16:45:52 +0100

    maj page telecharger

--- telecharger.php
+++ telecharger.php
@@ -8,7 +8,8 @@
 <section class="container">
     <div>
         <p>Après avoir choisi le type de support sur lequel vous souhaitez télécharger, vous serez rediriger vers la plateforme de téléchargement.</p>
-        
+        <a href="https://play.google.com/store?utm_source=emea_Med&utm_medium=hasem&utm_content=May2715&utm_campaign=Evergreen&pcampaignid=MKT-EG-emea-fr-all-Med-hasem-py-Evergreen-May2715-1%7cONSEM_kwid_43700007279174033&gref=EikQAhohChsKEwin0cOEnrjnAhXQ0N4KHcmfDnQQABgBIAASAs7T8P8HARjj9tyaAw&gclid=EAIaIQobChMIp9HDhJ645wIV0NDeCh3Jnw50EAAYASAAEgLO0_D_BwE&gclsrc=aw.ds" target="_blank"><img class="image" src="public/images/logo-android.png"></a>
+        <a href="https://www.apple.com/fr/ios/app-store/" target="_blank"><img class="image" src="public/images/logo-apple.png"></a>
     </div>
 
 </section>
