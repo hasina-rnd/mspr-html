bd6f4078dbcb7c398d10618a85cb52a8d1776b8f
hasina <hasinarandriamora97@gmail.com>
2020-02-04 09:23:10 +0100

    html

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include_once 'includes/header.php'; ?>
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -6,40 +8,6 @@
     <link rel="stylesheet" href="public/css/app.css">
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Télécharger</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Partenaires</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        A propos
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="A propos">
-                        <a class="dropdown-item" href="#">Equipe</a>
-                        <a class="dropdown-item" href="#">Origines</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-</header>
     <section>
             <h1>Pratum Application</h1>
         <div>
