217608f87d4b2a2b4a6b3a6f0b49bc6abe5674d2
louis <pavageau>
2020-02-05 14:25:31 +0100

    css contact

--- contact.php
+++ contact.php
@@ -7,35 +7,39 @@
 </section>
 <div class="container">
     <div class="contact">
-<h2>Formulaire de contact</h2>
-<div class="form-group"style="max-width: 500px">
-    <span for="name">Votre Nom</span>
-    <input class="form-control" name="name" id="name" placeholder="Entrez votre nom">
-</div>
+    <h2>Formulaire de contact</h2>
 
-<div class="form-group"style="max-width: 500px">
-    <span for="email">E-mail</span>
-    <input class="form-control" type="email" name="email" id="email" placeholder="Entrez votre e-mail">
-</div>
 
-    <div class="form-group"style="max-width: 500px">
-        <span for="siren">Numéro SIRET</span>
-        <input class="form-control" type="text" name="Siren"  placeholder="Entrez votre numéro SIRET">
+        <div class="container" >
+            <div class="form-group" style="max-width: 500px">
+                <span for="name">Votre Nom</span>
+                <input class="form-control" name="name" id="name" placeholder="Entrez votre nom">
+            </div>
+
+            <div class="form-group" style="max-width: 500px">
+                <span for="email">E-mail</span>
+                <input class="form-control" type="email" name="email" id="email" placeholder="Entrez votre e-mail">
+            </div>
+
+            <div class="form-group" style="max-width: 500px">
+                <span for="siren">Numéro SIRET</span>
+                <input class="form-control" type="text" name="Siren"  placeholder="Entrez votre numéro SIRET">
+            </div>
+
+            <div class="form-group" style="max-width: 500px">
+                <span for="téléphone">Numéro de téléphone</span>
+                <input class="form-control" type="text" name="téléphone"  placeholder="Entrez votre numéro de téléphone">
+            </div>
+
+            <div class="form-group" style="max-width: 500px">
+                <span for="message">Votre message</span>
+                <input class="form-control" type="textarea" name="message"  placeholder="Entrez votre message">
+            </div>
+        </div>
+
+        <button style="margin-bottom: 2em;" type="button" class="btn btn-outline-success">Envoyer</button>
     </div>
-
-    <div class="form-group"style="max-width: 500px">
-        <span for="téléphone">Numéro de téléphone</span>
-        <input class="form-control" type="text" name="téléphone"  placeholder="Entrez votre numéro de téléphone">
-    </div>
-
-    <div class="form-group"style="max-width: 500px">
-        <span for="message">Votre message</span>
-        <input class="form-control" type="textarea" name="message"  placeholder="Entrez votre message">
-    </div>
-</div>
-
-<button style="margin-bottom: 25px;" type="button" class="btn btn-outline-success">Envoyer</button>
-</div>
-
 <?php include_once 'includes/footer.php'; ?>
+
+
 
