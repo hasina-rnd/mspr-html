4c6af27043363bdcc790fde2605e759280b4ef14
Sébastien NICOU <snicou.pro@gmail.com>
2020-02-05 11:34:20 +0100

    add: gitignore

--- telecharger.php
+++ telecharger.php
@@ -1,15 +1,16 @@
 <?php include_once 'includes/header.php'; ?>
 <section class="jumbotron text-light" style="background-image: url(/mspr-html/images/pelouse.JPG)">
     <div>
-        <h1 class="display-4">Téléchargement de l'application</h1>
-        <p class="lead">Vous allez pouvoir télécharger l'application.</p>
+        <h1 class="display-4">Télécharger l'application</h1>
+        <p class="lead">L'application est disponible dès maintenant au téléchargement ! Cliquez sur le Store fe votre choix :</p>
     </div>
 </section>
 <section class="container">
     <div>
         <p>Après avoir choisi le type de support sur lequel vous souhaitez télécharger, vous serez rediriger vers la plateforme de téléchargement.</p>
-        <a href="https://play.google.com/store?utm_source=emea_Med&utm_medium=hasem&utm_content=May2715&utm_campaign=Evergreen&pcampaignid=MKT-EG-emea-fr-all-Med-hasem-py-Evergreen-May2715-1%7cONSEM_kwid_43700007279174033&gref=EikQAhohChsKEwin0cOEnrjnAhXQ0N4KHcmfDnQQABgBIAASAs7T8P8HARjj9tyaAw&gclid=EAIaIQobChMIp9HDhJ645wIV0NDeCh3Jnw50EAAYASAAEgLO0_D_BwE&gclsrc=aw.ds" target="_blank"><img class="image" src="public/images/logo-android.png"></a>
         <a href="https://www.apple.com/fr/ios/app-store/" target="_blank"><img class="image" src="public/images/logo-apple.png"></a>
+        <a href="https://play.google.com/store?utm_source=emea_Med&utm_medium=hasem&utm_content=May2715&utm_campaign=Evergreen&pcampaignid=MKT-EG-emea-fr-all-Med-hasem-py-Evergreen-May2715-1%7cONSEM_kwid_43700007279174033&gref=EikQAhohChsKEwin0cOEnrjnAhXQ0N4KHcmfDnQQABgBIAASAs7T8P8HARjj9tyaAw&gclid=EAIaIQobChMIp9HDhJ645wIV0NDeCh3Jnw50EAAYASAAEgLO0_D_BwE&gclsrc=aw.ds" target="_blank"><img class="image" src="public/images/logo-android.png"></a>
+        
     </div>
 
 </section>
