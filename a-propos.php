a5fcaaf107b4f47a212db958283c76b7ce4e6048
hasina <hasinarandriamora97@gmail.com>
2020-02-05 15:10:27 +0100

    maj page equipe

--- a-propos.php
+++ a-propos.php
@@ -9,15 +9,27 @@
     </div>
 
 </section>
+
 <section>
         <div class="container">
-            <h2>Les origines</h2>
-            <p>L'histoire commence à partir d'une discussion entre amis pendant que </p>
+            <h2>Notre application</h2>
+            <p class="lead">En règle générale, on n'y pense pas mais la pelouse d'un terrain de football joue un rôle important dans le déroulant d'un match.
+            Le jeu d'une équipe de football est dépendant de la qualité d'une pelouse. Pourtant le monde du football et le monde du paysagisme semble toujours se situer dans 2 univers différents.
+
+            <p class="lead">Aujourd'hui, nous avons pour objectif de donner la possibilité aux sociétés de jardiniers spécialisées dans l'entretien des pelouse d'obtenir une visibilité sur cette application.
+            Cette outil sera transmis et présenté à tous les clubs de football en France et en Angleterre.</p>
         </div>
         <div class="container">
-            <h2>Le football</h2>
-            <p>Texte sur le football et le lien avec les terrains de foot</p>
-            <p>IMAGE</p>
+            <h2>Les origines</h2>
+            <p class="lead">L'histoire commence à partir d'une discussion entre amis pendant que le Paris Saint Germain écrasait le FC Barcelone
+            4 à 0. Vous connaissez ces questions existentielles qui animent les soirées entre amis : quel type de sociétés s'occupe de cette belle pelouse parisienne ?
+            Nous avons décidé alors de se positionner sur cette interrogation afin d'obtenir des réponses fiables et logiques.</p>
+            <p class="lead">Après des enquêtes bien poussées - sur Google -, nous avons pensé qu'à partir de cette fameuse problématique peut s'ouvrir à nous un projet intéressant.
+            Nous avons alors décidé de prendre notre téléphone et de passer de nombreux coups de fil qui nous mèneront vers la création de cette application mobile</p>
+
+        </div>
+        <div class="container">
+            <img class="image" src="public/images/ballon-.jpg" alt="photo de ballon de foot sur une pelouse"
         </div>
 </section>
 
