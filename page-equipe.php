dea4e2d0838aae76164b950c615108c1156f0e48
hasina <hasinarandriamora97@gmail.com>
2020-02-05 10:25:59 +0100

    page equipe

--- page-equipe.php
+++ page-equipe.php
@@ -1,6 +1,43 @@
 <?php include_once 'includes/header.php'; ?>
+<section class="jumbotron text-light" style="background-image: url(/mspr-html/images/pelouse.JPG)">
+    <div>
+        <h1 class="display-4">L'équipe</h1>
+        <p class="lead">Nous sommes à l'origine de la création de cette initiative collective.</p>
+        <a href="telecharger.php"><button type="button" class="btn btn-success">Télécharger l'application</button></a>
+    </div>
+</section>
 
+<section class="container">
+    <div>
+        <h2>Les 3 passionnés de football de Pratum</h2>
+        <p class="lead">Le football nous passionne depuis notre plus jeune âge. nous sommes 3 gamins en charge de la création d'une application qui va faire plaisir aux amateurs de football. Nombreux sont ceux qui jouent au foot sur des terrains boueux. Nombreux sont ceux qui des terrains en terre.
+        Nous voulons réaliser cette application dans l'objectif de rendre service à ces jardiniers et ces clubs de football qui réussiront à se joindre beaucoup plus facilement. Le projet a été mis en avant grâce à 3 jeunes entrepreneurs efficaces et solides comme Ngolo Kanté.</p>
+    </div>
+</section>
 
+<section class="container">
+    <div>
+        <h2>Sebastien NICOU</h2>
+        <img src="" alt="Photo de Sebastien NICOU">
+        <p class="lead">Ancien joueur de l'US Vertou, Seb est notre premier joueur. Tactique mais aussi technique, il allie intelligence de jeu et technique balle aux pieds.
+        Ce joueur travaille dans l'ombre mais permet de gagner les matchs comme personne. Guardiola le voudrait dans son équipe grâce à sa vision du jeu, il serait le premier concurrent d'un Busquets au FC Barcelone.
+        Vous l'avez compris, ce joueur est le passeur décisif de cette équipe. Il est tout simplement le meuneur de jeu.
+        </p>
+    </div>
+</section>
+
+<section class="container">
+    <div>
+        <h2>Louis Pavageau</h2>
+        <img src="" alt="Photo de Louis Pavageau">
+        <p class="lead">
+            Connaissez vous Sergio Aguero ? Luis Suarez ? Louis est notre attaquant, notre buteur. Ce joueur est un joueur qui patiente près du buteur pour conclure une occasion.
+            Il réussit à s'éclipser pour apparaître au dernier moment pour surprendre tout le monde et mettre le but de la victoire à la dernière minute.
+            Attention, ce joueur travaille aussi pour l'équipe car le premier attaquant et le premier défenseur. Il participe aussi au jeu de l'équipe, à la construction d'une occasion grâce à ses
+            capacités techniques. Un vrai phénomène #RonaldoElPhenomeno.
+        </p>
+    </div>
+</section>
 
 
 
