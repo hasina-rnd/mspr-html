f3190a80d3c3b9dbd3f4a4236bc5779b67a3da27
Sébastien NICOU <snicou.pro@gmail.com>
2020-02-05 11:47:40 +0100

    logo

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,9 @@
 <body>
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="./index.php">Pratum</a>
+        <a href="index.php">
+            <img src="public/images/pratum-logo.png" alt="Ceci est logo" title="Logo PRATUM" class="logo">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
