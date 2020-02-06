2778f671eb3982d555e2412a7f8780c15b634c80
hasina <hasinarandriamora97@gmail.com>
2020-02-06 13:11:02 +0100

    partenaire maj

--- partenaires.php
+++ partenaires.php
@@ -9,25 +9,25 @@
     </section>
 
     <section class="container">
-        <div class="partenaires" style="text-align: center">
-                    <img src="public/images/logo-partenaires.svg" class="card-img-top" alt="logo-partenaires" style="max-width: 400px">
-                    <div class="space" style="margin-top: 25px">
-                    <p>Sur cette image vous pouvez voir quelles sont les partenaires avec qui nous travaillons au quotidien.</p>
-                    <p>Beaucoup de nos partenaires sont dans le milieu du sport comme Nike, la Lfp. Mais aussi dans le milieu de l’entretien des pelouses comme Jardiland.</p>
-                </div>
+        <div class="row">
+            <div class="col">
+                <img class="image-logo" src="public/images/logos.png">
+            </div>
         </div>
     </section>
-        <div class="container">
-            <div class="space" style="margin-top: 50px">
-
-            <h3>Les avantages de devenir partenaires</h3>
-                <ul>
-                    <li>La présence aux différents réunions</li>
-                    <li>Etre au courant des comptes de la société</li>
-                    <li>Se faire connaître du grand public</li>
-                    <li>Acquérir de nouveaux clients</li>
-                </ul>
+    <section class="container">
+        <div>
+            <h2>Accompagnés des meilleurs</h2>
+            <p class="lead">Nous sommes heureux de vous présenter nos partenaires. Pour réaliser un tel projet, il faut savoir s'entourer des meilleurs.
+            Aujourd'hui, nous sommes honorés de travailler avec de tels groupes. C'est un privilège que beaucoup aimeraientt avoir. Nous ne vous les présentons plus mais nous voulions leur offrir cette page afin de les remercier.
+            </p>
         </div>
+        <div>
+            <h2>Collaborons ensemble !</h2>
+            <p>Vous souhaitez collaborer ? Vous avez des idées qui visent à améliorer nos performances ? Ne soyez pas timides et envoyez nous vos suggestions sur <a href="./contact.php">notre page contact</a>.</p>
         </div>
-
+        <div>
+            
+        </div>
+    </section>
 <?php include_once 'includes/footer.php'; ?>
