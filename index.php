3e9ad1b5896c4eb7e317727b687d81041ae15550
hasina <hasinarandriamora97@gmail.com>
2020-02-07 10:42:28 +0100

    maj home page

--- index.php
+++ index.php
@@ -9,41 +9,47 @@
 </section>
 <section>
     <h3 class="container">Une offre claire et efficace, à votre service !</h3>
-    <img class="image-download" src="public/images/download-pratum.png" alt="download-pratum"
-         style="margin-left: 850px">
 </section>
 <section class="container">
     <div class="row">
-        <div class="col-sm-6" style="margin-top: -330px">
-            <div class="card-price">
-                <div class="card-body">
-                    <h5 class="card-title">PRATUM Starter | <strong>299,99€/mois</strong></h5>
-                    <p class="card-text">Entretien d'une pelouse pour la saison entière. Engagement pendant 12 mois.
-                        <br>LE PREMIER MOIS EST OFFERT</p>
-                    <a href="./telecharger.php" class="btn btn-primary">Je télécharge l'application</a>
-                </div>
+        <div class="col">
+    <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="public/images/download-pratum.png" alt="Card image cap">
+        <div class="card-body">
+                <h5 class="card-title">PRATUM Starter | <strong>299,99€/mois</strong></h5>
+                <p class="card-text">Entretien d'une pelouse pour la saison entière. Engagement pendant 12 mois. LE PREMIER MOIS EST OFFERT</p>
+                <a href="./telecharger.php" class="btn btn-primary">Je télécharge l'application</a>
+            </div>
+            </div>
+            </div>
+    </div>
+</section>
+
+
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>Un projet collaboratif et innovant dans le monde du Football</h2>
+                <p class="lead">On imagine que le monde du football est un monde fermé et hostile. On peut détruire ce cliché
+                grâce à cette application. Des jeunes jardiniers peuvent réaliser leurs rêves en collaborant avec les clubs
+                de Football de leur rêve. On leur offre la possibilité de voir de près leurs idoles.</p>
+                <a href="le-concept.php" class="btn btn-primary">Quelle est l'origine ?</a>
             </div>
         </div>
     </div>
 </section>
 
-
 <section class="py-5">
     <div class="container">
-        <h2>Un projet collaboratif et innovant dans le monde du Football</h2>
-        <p class="lead">On imagine que le monde du football est un monde fermé et hostile. On peut détruire ce cliché
-            grâce à cette application. Des jeunes jardiniers peuvent réaliser leurs rêves en collaborant avec les clubs
-            de Football de leur rêve. On leur offre la possibilité de voir de près leurs idoles.</p>
-        <a href="le-concept.php" class="btn btn-primary">Quelle est l'origine ?</a>
-    </div>
-</section>
-
-<section class="py-5">
-    <div class="container">
+        <div class="row">
+            <div class="col">
         <h2>L'équipe</h2>
         <p class="lead">Nous sommes des fans de Football. Nous continuons de travailler afin de proposer un service de
             qualité et une application à la hauteur du projet.</p>
         <a href="./page-equipe.php" class="btn btn-primary">Qui se cache derrière ce concept ?</a>
+            </div>
+        </div>
     </div>
 </section>
 
